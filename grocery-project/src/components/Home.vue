e1d2a3b64ebc02ba367862cae1faad3494dd417f
anadikulkarni <anadikulkarni@gmail.com>
2023-10-04 18:19:13 +0530

    first look

---
+++ grocery-project/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+<div class='homepage'>
+    <button @click="register"> Register </button>
+    <button @click="loginAdmin"> Admin Login </button>
+    <button @click="loginManager"> Manager Login </button>
+    <button @click="loginUser"> User Login </button>
+</div>
+</template>
+
+<script>
+export default{
+    name: "HomePage",
+    methods: {
+        register(){
+            console.log('registered')
+        },
+        loginAdmin(){
+            console.log('admin logged in')
+        },
+        loginManager(){
+            console.log('manager logged in')
+        },
+        loginUser(){
+            console.log('user logged in')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
